a3dfe741ec84f83e4ca0a65487e0822937737a96
Julius Rajala <juliusrajala@gmail.com>
2020-01-26 19:57:58 +0200

    Minor change to comments.

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 // import remoteClient from '../api/remoteClient';
-import localClient from '../api/localClient';
+import localClient from './localClient';
 
 // This file serves a single entry point for the api.
 // Selecting here which client you're exporting makes sure all api-calls are done to the same location.
